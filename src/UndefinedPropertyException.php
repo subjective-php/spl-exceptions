496c04a4b99dd714a7db9ac0fbe35302b860d57a
chadicus <chadwickgray@gmail.com>
2015-07-31 13:48:21 -0400

    Add UndefinedPropertyException

---
+++ src/UndefinedPropertyException.php
@@ -0,0 +1,9 @@
+<?php
+namespace Chadicus\Spl\Exceptions;
+
+/**
+ * Exception throw when trying to access a non-existant property for a class.
+ */
+class UndefinedPropertyException extends \LogicException
+{
+}
