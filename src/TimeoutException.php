93afb327f24b87a1998ca35d9e780864fc732058
chadicus <chadwickgray@gmail.com>
2016-08-17 21:37:31 -0400

    Add TimeoutException

---
+++ src/TimeoutException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Chadicus\Spl\Exceptions;
+
+/**
+ * Thrown when the time allotted for a process or operation has expired.
+ */
+class TimeoutException extends \RuntimeException
+{
+}
