816ff2746c32ba3faa91dcf3db6fa768c8a35668
chadicus <chad.gray@traderinteractive.com>
2019-11-19 13:09:02 -0500

    Add NotImplementedException

---
+++ src/NotImplementedException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace SubjectivePHP\Spl\Exceptions;
+
+use RuntimeException;
+
+/**
+ * Exception thrown when a requested method or operation is not implemented. 
+ */
+class NotImplementedException extends RuntimeException
+{
+}
