b1efff4482b38addbe48dc8837116d9c0ef811b1
chadicus <chad.gray@traderinteractive.com>
2022-12-19 15:06:43 -0500

    Fix PHPCS errors

--- src/NotImplementedException.php
+++ src/NotImplementedException.php
@@ -5,7 +5,7 @@ namespace SubjectivePHP\Spl\Exceptions;
 use RuntimeException;
 
 /**
- * Exception thrown when a requested method or operation is not implemented. 
+ * Exception thrown when a requested method or operation is not implemented.
  */
 class NotImplementedException extends RuntimeException
 {
